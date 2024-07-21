5328db0e5a54b67ee276886018901c05f152e52b
mohitlamba2004 <mohit.lamba.ug22@nsut.ac.in>
2024-07-21 09:00:09 +0530

    Q3

--- src/BinarySearchQuestions.java
+++ src/BinarySearchQuestions.java
@@ -60,9 +60,12 @@ class Q2{   // floor :- Greatest number smaller or equal to target.
 }
 
  */
-
+/*
 class Q3{  //
     public static void main(String[] args) {
+        int[] arr = { -13,-12,-11,-10,-4,0,5,6,7,8,9,10,11,16,23};
+        int target = 15;
+        System.out.println(fun(arr, target));
 
     }
     static int fun(int[] a , int tar) {
@@ -83,5 +86,39 @@ class Q3{  //
             }
         }
         return start;
+    }
+}
+
+ */
+
+//Q3 :- Return Smallest letter greater than target.
+
+class small{
+    public static void main(String[] args) {
+        char[] arr = {'c','f','j','l'};
+        char target = 'f';
+        System.out.println(fun(arr,target));
+    }
+    static char fun(char[] a, char tar){
+        int start = 0;
+        int end = a.length -1;
+        while(start<= end){
+            int mid = start + (end - start)/2;
+            if(tar == a[mid]){
+                return a[mid+1];
+            }
+            if(tar < a[mid]){
+                end = mid -1;
+            }
+            if (tar > a[mid]) {
+                start = mid +1;
+            }
+        }
+        if(start == a.length){
+            return a[0];
+        }
+        else{
+            return a[start];
+        }
     }
 }
