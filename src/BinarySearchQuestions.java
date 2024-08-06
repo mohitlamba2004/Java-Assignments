cc8a8f28302c510a0744d027a6e605edac5d8830
mohitlamba2004 <mohit.lamba.ug22@nsut.ac.in>
2024-08-06 12:36:09 +0530

    q4

--- src/BinarySearchQuestions.java
+++ src/BinarySearchQuestions.java
@@ -178,29 +178,30 @@ class Q4{
     public static void main(String[] args) {
 
     }
-    static int fun(int[] a , int tar){
+    static int[] fun(int[] nums, int target){
+
+
+    }
+    static int search(int[] a , int tar , boolean findleft){
+        int ans = -1;
         int start = 0;
         int end = a.length-1;
         while(start<=end){
             int mid = start + (end - start)/2;
-            if(tar == a[mid]){
-                int start1 = 0;
-                int end1 = mid;
-                while(start1<=end1){
-                    int mid1 = start1 + (end1 - start1)/2;
-                    if(tar > a[mid1]){
-                        start1 = mid1 +1;
-                    }
-                    else{
-                        end1 = mid1 -1;
-                    }
-                }
-            }
+
             if(tar > a[mid]){
                 start = mid +1;
             }
-            else{
+            if(tar<a[mid]){
                 end = mid -1;
+            }
+            else{
+               if(findleft== true){
+                   end = mid -1;
+               }
+               else {
+                   start = mid +1;
+               }
             }
         }
         return -1;
